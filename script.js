72ec1cdf57258fd110e8d8ccb90cca1fd3e9a2c3
cara-coding <101016965+cara-coding@users.noreply.github.com>
2022-07-13 13:42:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const cards= document.querySelectorAll(".memory-card");
+
+let hasFlippedCard= false;
+let firstCard,secondCard;
+
+function flipCard() {
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+        hasFlippedCard= true;
+        firstCard=this;
+        }
+        else {
+            hasFlippedCard=false;
+            secondCard= this;
+
+            if(firstCard.dataset.framework === secondCard.dataset.framework) {
+                firstCard.removeEventListener("click", flipCard);
+                secondCard.removeEventListener("click", flipCard);
+            }
+            else {
+                setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                }, 1500);
+            }
+
+
+
+            // console.log(firstCard.dataset.framework);
+            // console.log(secondCard.dataset.framework);
+            
+
+        }
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
